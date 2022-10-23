5f3ded688ff7354571d5e11ffab6ae8c839c5786
Harwin14 <agungharwinn@gmail.com>
2022-10-23 23:12:11 +0700

    coba api

--- cobapi/app.js
+++ cobapi/app.js
@@ -3,39 +3,61 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { MongoClient } = require('mongodb');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
 
-var app = express();
+// Database Name
+const dbName = 'breadsdb';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+async function main() {
+  const url = 'mongodb://localhost:27017';
+  const client = new MongoClient(url);
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  var indexRouter = require('./routes/index');
+  var usersRouter = require('./routes/users')(db);
+  
+  var app = express();
+  
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'ejs');
+  
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
+  
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  return 'done.';
+}
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+main()
+  .then(console.log)
+  .catch(console.error)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
--- cobapi/routes/users.js
+++ cobapi/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+const collection = db.collection('documents');
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
